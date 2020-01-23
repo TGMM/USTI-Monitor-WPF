aea3034bd66da48a534ef82ef039009f47612ec7
Eliud de León <tgmm.1000@gmail.com>
2020-01-23 11:56:07 -0600

    Add temperature properties to bind to the TemperatureConsole UserControl.

--- SerialMonitorWPF/ViewModels/ShellViewModel.cs
+++ SerialMonitorWPF/ViewModels/ShellViewModel.cs
@@ -9,6 +9,73 @@ namespace SerialMonitorWPF.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        #region TemperatureProperties
+        private string _temperature1;
+        private string _temperature2;
+        private string _temperature3;
+        private string _temperature4;
+        private string _temperature5;
+        private string _temperature6;
 
+        public string Temperature1
+        {
+            get { return _temperature1 + "°"; }
+            set
+            {
+                _temperature1 = value;
+                NotifyOfPropertyChange(() => Temperature1);
+            }
+        }
+
+        public string Temperature2
+        {
+            get { return _temperature2 + "°"; }
+            set
+            {
+                _temperature2 = value;
+                NotifyOfPropertyChange(() => Temperature2);
+            }
+        }
+
+        public string Temperature3
+        {
+            get { return _temperature3 + "°"; }
+            set
+            {
+                _temperature3 = value;
+                NotifyOfPropertyChange(() => Temperature3);
+            }
+        }
+
+        public string Temperature4
+        {
+            get { return _temperature4 + "°"; }
+            set
+            {
+                _temperature4 = value;
+                NotifyOfPropertyChange(() => Temperature4);
+            }
+        }
+
+        public string Temperature5
+        {
+            get { return _temperature5 + "°"; }
+            set
+            {
+                _temperature5 = value;
+                NotifyOfPropertyChange(() => Temperature5);
+            }
+        }
+
+        public string Temperature6
+        {
+            get { return _temperature6 + "°"; }
+            set
+            {
+                _temperature6 = value;
+                NotifyOfPropertyChange(() => Temperature6);
+            }
+        }
+        #endregion
     }
 }
